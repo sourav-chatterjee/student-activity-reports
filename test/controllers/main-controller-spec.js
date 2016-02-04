173ff2dca2d7511ce9c6c63e06fe1dac7bd8409f
saptarshideysarkar <saptarshids.cn@gmail.com>
2016-02-04 11:48:04 +0530

    Test commit

--- test/controllers/main-controller-spec.js
+++ test/controllers/main-controller-spec.js
@@ -1,24 +1,22 @@
 'use strict';
 
-// xdescribe('Controller: src/js/controller/main-ctrl', function() {
-//
-//     var $rootScope, $scope, $controller;
-//
-//     beforeEach(module('studentActivityReports.home'));
-//
-//     beforeEach(inject(function(_$rootScope_, _$controller_) {
-//         $rootScope = _$rootScope_;
-//         $scope = $rootScope.$new();
-//         $controller = _$controller_;
-//
-//         $controller('MainCtrl', {'$rootScope' : $rootScope, '$scope': $scope});
-//     }));
-//
-//     it('should check the length of the array', function() {
-//         expect($scope.teacherArr.length).toEqual(3);
-//     });
-// });
-//
+xdescribe('Controller: src/js/controller/main-ctrl', function() {
+
+    var $rootScope, $scope, $controller;
+
+    beforeEach(module('studentActivityReports.home'));
+    beforeEach(inject(function(_$rootScope_, _$controller_) {
+        $rootScope = _$rootScope_;
+        $scope = $rootScope.$new();
+        $controller = _$controller_;
+         $controller('MainCtrl', {'$rootScope' : $rootScope, '$scope': $scope});
+    }));
+
+     it('should check the length of the array', function() {
+        expect($scope.teacherArr.length).toEqual(3);
+     });
+ }); 
+
 // xdescribe("A suite is just a function", function() {
 //   var a;
 //
